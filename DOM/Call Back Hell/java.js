0bcd491d0a8a5d96cb54947df7248f90536694c9
kavinthangavel245 <kavinthangavel245@gmail.com>
2025-07-10 14:28:15 +0530

    Simulated fake API requests using Promise chaining (page1 → page3)

--- DOM/Call Back Hell/java.js
+++ DOM/Call Back Hell/java.js
@@ -1,21 +1,58 @@
-const delayedColorChange = (newColor, delay, doNext) => {
-    setTimeout( () => {
-        document.body.style.backgroundColor = newColor;
-        console.log("changed to new color: " + newColor);
-        doNext && doNext();
-    },delay)
-}
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout( () => {
+//         document.body.style.backgroundColor = newColor;
+//         console.log("changed to new color: " + newColor);
+//         doNext && doNext();
+//     },delay)
+// }
 
-delayedColorChange("red",1000,() => {
-    delayedColorChange("blue",1000,() => {
-        delayedColorChange("green",1000,() => {
-            delayedColorChange("yellow",1000,() => {
-                delayedColorChange("pink",1000,() => {
-                    delayedColorChange("purple",1000,() => {
-                        console.log("All colors changed!");
-                    });
-                });
-            });
+// delayedColorChange("red",1000,() => {
+//     delayedColorChange("blue",1000,() => {
+//         delayedColorChange("green",1000,() => {
+//             delayedColorChange("yellow",1000,() => {
+//                 delayedColorChange("pink",1000,() => {
+//                     delayedColorChange("purple",1000,() => {
+//                         console.log("All colors changed!");
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// })
+
+/*fakeRequest Using Callbacks*/
+function fakeRequestPromise(url) {
+    console.log("Request made to: " + url);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = Math.random() > 0.3; // 70% chance of success
+            if (success) {
+                resolve(`Here is your fake data from ${url}`);
+            } else {
+                reject("Request failed!");
+            }
+        }, 1000);
         });
-    });
-})
+    }
+
+
+
+fakeRequestPromise('yelp.com/api/coffee/page1')
+            .then((data) => {
+                console.log("It worked! (page 1)");
+                return fakeRequestPromise('yelp.com/api/coffee/page2');
+                console.log(data); // This will log the data from page 1
+            })
+            .then((data) => {
+                console.log("It worked! (page 2)");
+                return fakeRequestPromise('yelp.com/api/coffee/page3');
+                console.log(data); // This will log the data from page 2
+            })
+            .then((data) => {
+                console.log("It worked! (page 3)");
+                console.log(data); // This will log the data from page 3
+            })
+            .catch((err) => {
+                console.log("A request failed ❌");
+                console.log("Error: ", err);
+            });
