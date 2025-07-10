bb88122fbe0d9e2b53204cbca8099a916392cc2e
kavinthangavel245 <kavinthangavel245@gmail.com>
2025-07-10 14:55:19 +0530

    Background color change sequence simplified using Promise chain

--- DOM/Call Back Hell/java.js
+++ DOM/Call Back Hell/java.js
@@ -21,38 +21,58 @@
 // })
 
 /*fakeRequest Using Callbacks*/
-function fakeRequestPromise(url) {
-    console.log("Request made to: " + url);
+// function fakeRequestPromise(url) {
+//     console.log("Request made to: " + url);
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const success = Math.random() > 0.3; // 70% chance of success
+//             if (success) {
+//                 resolve(`Here is your fake data from ${url}`);
+//             } else {
+//                 reject("Request failed!");
+//             }
+//         }, 1000);
+//         });
+//     }
+
+
+
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//             .then((data) => {
+//                 console.log("It worked! (page 1)");
+//                 return fakeRequestPromise('yelp.com/api/coffee/page2');
+//                 console.log(data); // This will log the data from page 1
+//             })
+//             .then((data) => {
+//                 console.log("It worked! (page 2)");
+//                 return fakeRequestPromise('yelp.com/api/coffee/page3');
+//                 console.log(data); // This will log the data from page 2
+//             })
+//             .then((data) => {
+//                 console.log("It worked! (page 3)");
+//                 console.log(data); // This will log the data from page 3
+//             })
+//             .catch((err) => {
+//                 console.log("A request failed ❌");
+//                 console.log("Error: ", err);
+//             });
+
+const delayedColorChange = (newColor, delay) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const success = Math.random() > 0.3; // 70% chance of success
-            if (success) {
-                resolve(`Here is your fake data from ${url}`);
-            } else {
-                reject("Request failed!");
-            }
-        }, 1000);
-        });
-    }
-
-
-
-fakeRequestPromise('yelp.com/api/coffee/page1')
-            .then((data) => {
-                console.log("It worked! (page 1)");
-                return fakeRequestPromise('yelp.com/api/coffee/page2');
-                console.log(data); // This will log the data from page 1
-            })
-            .then((data) => {
-                console.log("It worked! (page 2)");
-                return fakeRequestPromise('yelp.com/api/coffee/page3');
-                console.log(data); // This will log the data from page 2
-            })
-            .then((data) => {
-                console.log("It worked! (page 3)");
-                console.log(data); // This will log the data from page 3
-            })
-            .catch((err) => {
-                console.log("A request failed ❌");
-                console.log("Error: ", err);
-            });
+            document.body.style.backgroundColor = newColor;
+            console.log("changed to new color: " + newColor);
+            resolve();
+        }, delay);
+    });
+}
+    delayedColorChange("red",1000)
+        .then(() => delayedColorChange("blue", 1000))
+        .then(() => delayedColorChange("green", 1000))
+        .then(() => delayedColorChange("yellow", 1000)) 
+        .then(() => delayedColorChange("pink", 1000))
+        .then(() => delayedColorChange("purple", 1000))
+        .then(() => console.log("All colors changed!"))
+        .catch((err) => {   
+            console.error("An error occurred:", err);
+        });                 
